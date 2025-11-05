e197c3eba7643d328e053c8d8eb0a3cdc8bb98d5
khangnguyen-yay <khangnguyen@ucla.edu>
2025-11-04 19:37:52 -0800

    added filters

--- src/Card.tsx
+++ src/Card.tsx
@@ -10,12 +10,15 @@ type CardProps = {
   numMembers : number
   meetingTime : string
   location : string
+  filters : string[]
 }
 
 //Card component displaying club stuff
 //(no need to implement statuses yet just UI
 //place holder buttons for the different statuses)
-export function Card({name, category, description, numMembers, meetingTime, location} : CardProps) {
+
+//later: add assertions about width/contents of each parameter before displaying it (ex. isn't too long, etc)
+export function Card({name, category, description, numMembers, meetingTime, location, filters} : CardProps) {
     return (
         <div className="box">
           <h1 className="clubName">{name}</h1>
@@ -36,6 +39,15 @@ export function Card({name, category, description, numMembers, meetingTime, loca
             <img className="symbol" src={locationSymbol}></img>
             <div className="line">{location}</div>
           </div>
+
+          <div className="filtersContainer">
+            {filters.map((filter) => (
+              <div className="filterBox">
+                <div className="filterText"> {filter} </div>
+              </div>
+            ))}
+          </div>
+
           <div className="addToTracker">Add to tracker:</div>
           <div className="buttonBox">
             <button className="statusButton considerButton">Consider</button>
