5093c5077593e4ed3aa113df4fdbe8f76499d826
NIDHI <nidhipamnani@g.ucla.edu>
2025-11-11 11:48:38 -0800

    filter component is added. based the filtering off of the description

---
+++ frontend/src/filter-view/filter-view.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+interface CategoryFilterProps {
+  categories: string[];
+  selectedCategory: string;
+  onCategoryChange: (category: string) => void;
+}
+
+const CategoryFilter: React.FC<CategoryFilterProps> = ({
+  categories,
+  selectedCategory,
+  onCategoryChange,
+}) => {
+  return (
+    <select
+      className="border rounded-md p-2 mb-4"
+      value={selectedCategory}
+      onChange={(e) => onCategoryChange(e.target.value)}
+    >
+      {categories.map((cat) => (
+        <option key={cat} value={cat}>
+          {cat}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export default CategoryFilter;
