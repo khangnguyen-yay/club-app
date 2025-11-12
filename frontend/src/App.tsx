91d31f4394a2232cc16aa55f36966a9c4cecab57
Khang <khangnguyen@ucla.edu>
2025-11-11 23:43:22 -0800

    Added tab images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,10 @@ import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Calendar from "./pages/Calendar";
 
+import HomeLogo from './pages/NavigationUI/NavigationImages/home.svg';
+import ExploreLogo from './pages/NavigationUI/NavigationImages/explore.svg';
+import CalendarLogo from './pages/NavigationUI/NavigationImages/calendar.svg';
+
 import './pages/NavigationUI/Navigation.css';
 
 function App() {
@@ -12,9 +16,17 @@ function App() {
     <Router>
       <nav className="navLinks">
         {/* Simple navigation links */}
-        <NavLink to="/" className="link">Home</NavLink>
-        <NavLink to="/explore" className="link">Explore</NavLink>
-        <NavLink to="/calendar" className="link">Calendar</NavLink>
+        <NavLink to="/" className="link">
+          <img src={HomeLogo} className="navBarLogo"></img> Home
+        </NavLink>
+
+        <NavLink to="/explore" className="link">
+          <img src={ExploreLogo} className="navBarLogo"></img> Explore
+        </NavLink>
+      
+        <NavLink to="/calendar" className="link">
+        <img src={CalendarLogo} className="navBarLogo"></img> Calendar
+        </NavLink>
       </nav>
 
       <Routes>
