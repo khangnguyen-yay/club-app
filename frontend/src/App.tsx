dd2abc6ba29a5c4539e724c6fa4ca20defcb0570
Khang <khangnguyen@ucla.edu>
2025-11-11 22:57:00 -0800

    changed Links to NavLinks and added styling so that the tab the user selects turns white

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
 // Import your page components
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Calendar from "./pages/Calendar";
 
+import './pages/NavigationUI/Navigation.css';
+
 function App() {
   return (
     <Router>
-      <nav>
+      <nav className="navLinks">
         {/* Simple navigation links */}
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/explore">Explore</Link> |{" "}
-        <Link to="/calendar">Calendar</Link>
+        <NavLink to="/" className="link">Home</NavLink>
+        <NavLink to="/explore" className="link">Explore</NavLink>
+        <NavLink to="/calendar" className="link">Calendar</NavLink>
       </nav>
 
       <Routes>
