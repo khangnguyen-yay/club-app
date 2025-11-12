db032f3ae352c3f9a00f321bca8027db16de2ff3
Khang <khangnguyen@ucla.edu>
2025-11-12 00:36:20 -0800

    Created basic Home page outline

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,37 @@
 import React from "react";
+import './Home.css';
 import ClubList from "../components/testClubList";
+
 function Home() {
+
+  let categoryCount : number = 0;
+
+  let categoryDisplay : string = "No clubs in this category yet."
+
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Welcome to the Home page! This is the main landing page of your app.</p>
+
+      <div className="categoryBlock">
+        <span className="dot"></span>
+        <h2>Applied</h2>
+        <span className="countBox">{categoryCount}</span>
+      </div>
+      <h3>{categoryDisplay}</h3>
+
+      <div className="categoryBlock">
+        <span className="dot"></span>
+        <h2>Applying</h2>
+        <span className="countBox">{categoryCount}</span>
+      </div>
+      <h3>{categoryDisplay}</h3>
+
+      <div className="categoryBlock">
+        <span className="dot"></span>
+        <h2>Considering</h2>
+        <span className="countBox">{categoryCount}</span>
+      </div>
+      <h3>{categoryDisplay}</h3>
       <ClubList />
     </div>
   );
