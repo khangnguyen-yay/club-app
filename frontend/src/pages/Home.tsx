554bab60d21df503250ccf2b50ba1ebf415a04b5
Khang <khangnguyen@ucla.edu>
2025-11-29 02:14:21 -0800

    Added code so that first card appears under Applying on the home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,39 +1,56 @@
 import React from "react";
 import './Home.css';
+import CardList from '../appCard/CardList.tsx';
+import type { Club } from '../appCard/CardList.tsx';
 import TestStatus from "../components/testStatus";
+
 
 function Home() {
 
   let categoryCount : number = 0;
 
-  let categoryDisplay : string = "No clubs in this category yet."
+  let categoryDisplay : React.JSX.Element | string = "No clubs in this category yet."
 
-  let listStatus = "applied";
+  const testClub : Club = {
+    club_name: "Pilipinos In Engineering and Science",
+    type: "Academic",
+    ig: "https://www.instagram.com/pierrethepiebear/",
+    website: "https://piesucla.wixsite.com/pies",
+    notes : ''
+  }
+
+  categoryDisplay = <CardList filteredCards={[testClub]}></CardList>
 
   return (
     <div>
       <h1>Home Page</h1>
 
-      <div className="categoryBlock">
-        <span className="dot"></span>
-        <h2>Applied</h2>
-        <span className="countBox">{categoryCount}</span>
+      <div className="appliedSection">
+        <div className="categoryBlock">
+          <span className="dot"></span>
+          <h2>Applied</h2>
+          <span className="countBox">{categoryCount}</span>
+        </div>
+        <h3>{categoryDisplay}</h3>
       </div>
-      <h3>{categoryDisplay}</h3>
 
-      <div className="categoryBlock">
-        <span className="dot"></span>
-        <h2>Applying</h2>
-        <span className="countBox">{categoryCount}</span>
+      <div className="applyingSection">
+        <div className="categoryBlock">
+          <span className="dot"></span>
+          <h2>Applying</h2>
+          <span className="countBox">{categoryCount}</span>
+        </div>
+        <h3>{categoryDisplay}</h3>
       </div>
-      <h3>{categoryDisplay}</h3>
+      <div className="considerSection">
+        <div className="categoryBlock">
+          <span className="dot"></span>
+          <h2>Considering</h2>
+          <span className="countBox">{categoryCount}</span>
+        </div>
+        <h3>{categoryDisplay}</h3>
+      </div>
 
-      <div className="categoryBlock">
-        <span className="dot"></span>
-        <h2>Considering</h2>
-        <span className="countBox">{categoryCount}</span>
-      </div>
-      <h3>{categoryDisplay}</h3>
       <TestStatus />
     </div>
   );
