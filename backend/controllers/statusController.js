b207c472ef39c45992eb0f6af2bbc169ed820b46
Camillalalala <camilla4clubs@gmail.com>
2025-11-30 23:50:51 -0800

    Updated documentation for modifyUserClubStatus

--- backend/controllers/statusController.js
+++ backend/controllers/statusController.js
@@ -1,5 +1,4 @@
 import { getUserClubsByStatus, getAllUserClubsWithStatus, addUserClubPreference, updateUserClubPreference, getUserClubPreference } from "../services/statusService.js";
-/*NOTE: "status" and "preference" are used interchangeably*/
 
 export const getUserClubStatus = async (req, res) => {
     try {
@@ -22,27 +21,30 @@ export const getUserClubStatus = async (req, res) => {
     }
 };
 
+/*
+NOTE: "Status" and "Preference" are used interchangeably. To make it easier to differentiate between
+functions for GET and POST requests, "Status" is used in function names for GET requests, while "Preference" is 
+used in function names for POST requests.
+*/
 export async function modifyUserClubStatus(req, res) {
-  // User object is stored in req.user after user deserialization by Passport
-  const userId = req.user?.id || req.user?.google_id;
+  // req.user only exists if user has been authenticated via ensureAuth middleware; 
+  // optional chaining allows for userId to evaluate as undefined rather than throwing a runtime error
+  const userId = req.user?.id;
   const { clubId, preference } = req.body;
 
-  //Validation
-  if (!userId) return res.status(401).json({ error: 'Unauthorized' });
+  if (!userId) return res.status(401).json({ error: 'User is not authenticated' });
   if (!clubId) return res.status(400).json({ error: 'Missing clubId in request body' });
   if (!preference) return res.status(400).json({ error: 'Missing preference in request body' });
  
   try {
-    // If the user already has a preference for this club
+    // See if user already has an existing preference for a club to determine whether to add or update the club preference
     const existing = await getUserClubPreference(userId, clubId);
-
-    //Determine action: update existing preference or add a new one
     const result = existing
       ? await updateUserClubPreference(userId, clubId, preference)
       : await addUserClubPreference(userId, clubId, preference);
-    
-    //Send Response
+
     return res.json({ message: existing ? "Status updated" : "Status added", ...result });
+
   } catch (err) {
     console.error("modifyUserClubStatus error: ", err);
     return res.status(500).json({ error: "Server error" });
