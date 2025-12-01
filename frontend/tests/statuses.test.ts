bfc728373a4f2bdf8fa05553abba3197b5f6ef96
Khang <khangnguyen@ucla.edu>
2025-11-30 21:26:40 -0800

    Added test - selecting applying button adds club to applying category in backend

--- frontend/tests/statuses.test.ts
+++ frontend/tests/statuses.test.ts
@@ -28,3 +28,25 @@ test('Selecting applied button adds club to applied category in backend', async 
     const clubIDToCheck = 1;
     expect(appliedClubs.some((club : Club) => club.clubID === clubIDToCheck)).toBe(true);
   });
+
+  test('Selecting applying button adds club to applying category in backend', async ({ page }) => {
+    // Go to explore page
+    await page.goto('http://localhost:5173/explore');
+
+    //Click applied button that is currently unselected
+    const appliedButton = await page.locator('.applyingButton').first();
+    await expect(appliedButton).toHaveCSS('background-color', 'rgb(255, 165, 0)');
+    await appliedButton.click();
+
+    const updatedApplyingButtons = page.locator('.statusButton.applyingButtonClicked');
+
+    const updatedApplyingButton = await updatedApplyingButtons.first();
+    await expect(updatedApplyingButton).toHaveCSS('background-color', 'rgb(194, 126, 0)');
+    // Check that it is now in backend
+    //const appliedClubs = await fetchClubsByStatus("applied"); this didn't work
+    const res = await page.request.get('http://localhost:3000/api/user/clubs?status=applying');
+    const applyingClubs : Club[] = await res.json();
+    const clubIDToCheck = 1;
+    console.log(applyingClubs);
+    expect(applyingClubs.some((club : Club) => club.clubID === clubIDToCheck)).toBe(true);
+  });
