f6e7cec121c5b26a8ba5619f693f79b0ab885184
Khang <khangnguyen@ucla.edu>
2025-12-03 20:48:32 -0800

    Updated explore button tests to accomodate new color scheme. Added reset code to the Before hook so that cards reset before each test

--- frontend/tests/hooks.ts
+++ frontend/tests/hooks.ts
@@ -1,9 +1,48 @@
 import { Before, After  } from '@cucumber/cucumber';
+import { chromium } from 'playwright';
+import path from 'path';
 
-Before({ timeout: 10000 }, async function () {
-  await this.init();
-  this.firstButton = this.page.locator('.applyingButton').first();
-  await this.firstButton.waitFor({ state: 'visible', timeout: 10000 });
+
+Before({ timeout: 30000 }, async function () {
+
+  // Launch browser once per worker
+  if (!this.browser) {
+    this.browser = await chromium.launch({ headless: false });
+  }
+
+  // Load Google session
+  this.context = await this.browser.newContext({
+    storageState: path.resolve('storageState.json')   // remove './' (safer)
+  });
+
+  this.page = await this.context.newPage();
+
+  // If your World has init() defined
+  if (this.init) {
+    await this.init();
+  }
+
+  //Reset the cards
+  await this.page.goto('http://localhost:5173/home');
+
+  //iterate through selected applied buttons and unselect them
+  const updatedAppliedButtons = await this.page.locator('.statusButton.appliedButtonClicked');
+  while (await updatedAppliedButtons.count() > 0) {
+      await updatedAppliedButtons.first().click();
+  }
+
+  //iterate through applying buttons and unselect them
+  const updatedApplyingButtons = await this.page.locator('.statusButton.applyingButtonClicked');
+  while (await updatedApplyingButtons.count() > 0) {
+      await updatedApplyingButtons.first().click();
+  }
+
+  //iterate through consider buttons and unselect them
+  const updatedConsiderButtons = await this.page.locator('.statusButton.considerButtonClicked');
+  while (await updatedConsiderButtons.count() > 0) {
+      await updatedConsiderButtons.first().click();
+  }
+
 });
 
 After(async function () {
